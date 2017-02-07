22f1fffe0554ac8df565f41bb0f8c65f76abb292
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-07 21:04:46 +0000

    Added condition for different rendering of server messages.

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,12 +3,22 @@ import React, {Component} from 'react';
 class Message extends Component {
   render() {
     const messages = this.props.messages;
-    const messageItems = messages.map((message) =>
-      <div key={message.id} className="message">
-        <span className="message-username">{message.username}</span>
-        <span className="message-content">{message.content}</span>
-      </div>
-    );
+    const messageItems = messages.map((message) => {
+      if (message.type === "incomingMessage") {
+        return (
+          <div key={message.id} className="message">
+            <span className="message-username" style={{color: message.colour}}>{message.username}</span>
+            <span className="message-content">{message.content}</span>
+          </div>
+        );
+      } else {
+        return (
+          <div key={message.id} className="message system">
+            <span className="message-content">{message.content}</span>
+          </div>
+        );
+      }
+    });
 
     return (
       <div>{messageItems}</div>
