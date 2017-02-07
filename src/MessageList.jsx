2210083e520b02f2db5a3fbb7c4e7032f479f4b7
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-07 17:14:49 +0000

    Created MessageList that holds Message components.

---
+++ src/MessageList.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Message from "./Message.jsx";
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div>
+        <main className="messages">
+          <Message messages={this.props.messages} />
+        </main>
+      </div>
+    );
+  }
+}
+export default MessageList;
