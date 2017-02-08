b7022398eca1b6b55bc3cdba7f48003cfb0dfcc9
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-08 00:09:59 +0000

    Added logic to replace img urls with images in messages.

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,13 +5,28 @@ class Message extends Component {
     const messages = this.props.messages;
     const messageItems = messages.map((message) => {
       if (message.type === "incomingMessage") {
+
+        let newContent = [message.content];
+
+        if (message.hasImages) {
+          newContent = message.content.split(" ")
+
+          newContent = newContent.map((item, index) => {
+            if (item.match(/.+(\.png|\.jpg|\.gif)/)) {
+              return ( <img key={index} className="message-image" src={item} />) ;
+            }
+            return item;
+          })
+        }
+
         return (
           <div key={message.id} className="message">
             <span className="message-username" style={{color: message.colour}}>{message.username}</span>
-            <span className="message-content">{message.content}</span>
+            <span className="message-content">{newContent}</span>
           </div>
         );
-      } else {
+      }
+      if (message.type ==="incomingNotification" ) {
         return (
           <div key={message.id} className="message system">
             <span className="message-content">{message.content}</span>
