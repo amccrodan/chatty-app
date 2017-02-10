66f16df7e0e7c8336f7356abd8838635f3100094
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-10 23:46:14 +0000

    Brought in logic to decide what type of message is coming in, to then render a Message component or a notification.

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,10 +4,56 @@ import ReactDOM from 'react-dom';
 
 class MessageList extends Component {
   render() {
+
+    const messages = this.props.messages;
+    const messageItems = messages.map((message, index) => {
+      if (message.type === "incomingMessage") {
+        let newMessage = message;
+
+        let speechContent = [message.content];
+
+        if (message.speech) {
+          speechContent = message.content.substring(5);
+          newMessage.content = speechContent;
+          if (index === messages.length - 1) {
+            const utterance = new SpeechSynthesisUtterance(speechContent);
+            utterance.rate = 0.4;
+            utterance.pitch = 2.5;
+            window.speechSynthesis.cancel();
+            window.speechSynthesis.speak(utterance);
+          }
+        }
+
+        if (message.hasImages) {
+          const contentArr = message.content.split(" ")
+
+          newMessage.content = contentArr.map((item, index) => {
+            if (item.match(/.+(\.png|\.jpg|\.gif)/)) {
+              return ( <img key={index} className="message-image" src={item} />) ;
+            }
+            return item + " ";
+          })
+
+          message.hasImages = false;
+        }
+
+        return (
+          <Message key={message.id} message={newMessage} />
+        );
+      }
+      if (message.type === "incomingNotification" ) {
+        return (
+          <div key={message.id} className="message system">
+            <span className="message-content">{message.content}</span>
+          </div>
+        );
+      }
+    });
+
     return (
       <div>
         <main className="messages">
-          <Message messages={this.props.messages} />
+          {messageItems}
         </main>
         <div style={ {float:"left", clear: "both"} }
           ref={(el) => { this.messagesEnd = el; }}>
