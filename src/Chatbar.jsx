420d0900ddd60308a53ed04078446d3606403190
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-10 23:43:50 +0000

    Added local functions to handle keyUps and call the relevant App.jsx functions.

--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -3,6 +3,21 @@ import React, {Component} from 'react';
 class Chatbar extends Component {
   constructor(props) {
     super(props);
+    this.handleUsernameKeyUp = this.handleUsernameKeyUp.bind(this);
+    this.handleMessageKeyUp = this.handleMessageKeyUp.bind(this);
+  }
+
+  handleUsernameKeyUp (event) {
+    if(event.keyCode === 13) {
+      this.props.updateUsername(event.target.value);
+    }
+  }
+
+  handleMessageKeyUp (event) {
+    if(event.keyCode === 13) {
+      this.props.addMessage(event.target.value);
+      event.target.value = "";
+    }
   }
 
   render() {
@@ -12,11 +27,11 @@ class Chatbar extends Component {
           className="chatbar-username"
           placeholder="Your Name (Optional)"
           defaultValue={this.props.currentUser}
-          onKeyUp={this.props.updateUsername}/>
+          onKeyUp={this.handleUsernameKeyUp}/>
         <input
           className="chatbar-message"
           placeholder="Type a message and hit ENTER"
-          onKeyUp={this.props.addMessage}/>
+          onKeyUp={this.handleMessageKeyUp}/>
       </footer>
     );
   }
