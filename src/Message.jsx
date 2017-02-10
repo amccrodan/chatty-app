9bc6e59913597ffde4eefac8da8d555b199e4b06
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-10 23:45:31 +0000

    Moved the .map out to MessageList so that Message only renders a configured Message.

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,52 +2,11 @@ import React, {Component} from 'react';
 
 class Message extends Component {
   render() {
-    const messages = this.props.messages;
-    const messageItems = messages.map((message, index) => {
-      if (message.type === "incomingMessage") {
-
-        let newContent = [message.content];
-
-        if (message.speech) {
-          newContent = message.content.substring(5);
-          if (index === messages.length - 1) {
-            const utterance = new SpeechSynthesisUtterance(newContent);
-            utterance.rate = 0.4;
-            utterance.pitch = 2.5;
-            window.speechSynthesis.cancel();
-            window.speechSynthesis.speak(utterance);
-          }
-        }
-
-        if (message.hasImages) {
-          newContent = message.content.split(" ")
-
-          newContent = newContent.map((item, index) => {
-            if (item.match(/.+(\.png|\.jpg|\.gif)/)) {
-              return ( <img key={index} className="message-image" src={item} />) ;
-            }
-            return item + " ";
-          })
-        }
-
-        return (
-          <div key={message.id} className="message">
-            <span className="message-username" style={{color: message.colour}}>{message.username}</span>
-            <span className="message-content">{newContent}</span>
-          </div>
-        );
-      }
-      if (message.type === "incomingNotification" ) {
-        return (
-          <div key={message.id} className="message system">
-            <span className="message-content">{message.content}</span>
-          </div>
-        );
-      }
-    });
-
     return (
-      <div>{messageItems}</div>
+      <div className="message">
+        <span className="message-username" style={{color: this.props.message.colour}}>{this.props.message.username}</span>
+        <span className="message-content">{this.props.message.content}</span>
+      </div>
     );
   }
 }
